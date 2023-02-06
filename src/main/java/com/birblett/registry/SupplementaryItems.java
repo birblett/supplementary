6920707386b5638814e6daa000ef15a6196dcfdb
Vex <pintofoil7@gmail.com>
2023-02-05 23:31:37 -0800

    Steel helmet registered and textures added, plate armor material and platesteel item added

--- src/main/java/com/birblett/registry/SupplementaryItems.java
+++ src/main/java/com/birblett/registry/SupplementaryItems.java
@@ -1,9 +1,11 @@
 package com.birblett.registry;
 
+import com.birblett.armor_materials.steel_plate.SteelPlateItem;
 import com.birblett.trinkets.CapeItem;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import com.birblett.armor_materials.steel_plate.SteelPlateArmorMaterial;
 import net.minecraft.item.ArmorMaterial;
+import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
@@ -15,10 +17,10 @@ public class SupplementaryItems {
 
     // steel armor set
     public static final ArmorMaterial STEEL_PLATE_ARMOR_MATERIAL = new SteelPlateArmorMaterial();
-    public static final Item STEEL_HELMET = new Item(new FabricItemSettings());
-    public static final Item STEEL_CHESTPLATE = new Item(new FabricItemSettings());
-    public static final Item STEEL_LEGGINGS = new Item(new FabricItemSettings());
-    public static final Item STEEL_BOOTS = new Item(new FabricItemSettings());
+    public static final Item STEEL_HELMET = new SteelPlateItem(STEEL_PLATE_ARMOR_MATERIAL, EquipmentSlot.HEAD, new Item.Settings());
+    public static final Item STEEL_CHESTPLATE = new SteelPlateItem(STEEL_PLATE_ARMOR_MATERIAL, EquipmentSlot.CHEST, new Item.Settings());
+    public static final Item STEEL_LEGGINGS = new SteelPlateItem(STEEL_PLATE_ARMOR_MATERIAL, EquipmentSlot.LEGS, new Item.Settings());
+    public static final Item STEEL_BOOTS = new SteelPlateItem(STEEL_PLATE_ARMOR_MATERIAL, EquipmentSlot.FEET, new Item.Settings());
 
     public static final Item CAPE = new CapeItem(new FabricItemSettings().group(ItemGroup.MISC).maxCount(1));
 
