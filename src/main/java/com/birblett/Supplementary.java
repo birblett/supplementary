4df9bb972d22ab80b7a3f87f997afa6b5b37cf42
birblett <bwee.sylv@gmail.com>
2023-02-17 00:06:12 -0800

    add vector helper class

--- src/main/java/com/birblett/Supplementary.java
+++ src/main/java/com/birblett/Supplementary.java
@@ -9,8 +9,11 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Random;
+
 public class Supplementary implements ModInitializer {
 
+	public static final Random SUPPLEMENTARY_RANDOM = new Random();
 	public static final Logger LOGGER = LoggerFactory.getLogger("supplementary");
 	public static final String MODID = "supplementary";
 
